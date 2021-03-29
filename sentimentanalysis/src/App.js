e1a7d9ff7e9eb9c4d1c83dc3228bde91c95b547d
sharozraees802 <sharozraees802@gmail.com>
2021-03-29 16:46:09 +0500

    sentimental anal,ysis with star

--- sentimentanalysis/src/App.js
+++ sentimentanalysis/src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 
 import "./App.css";
 import Sentiment from "sentiment";
+import StarRatings from 'react-star-ratings';
 
 function App() {
   const sentiment = new Sentiment();
   const [sentimentalScore, setSentimentalScore] = useState(null);
   const [genealsentiment, setGenealsentiment] = useState(null);
   const [sentimenttext, setSentimenttext] = useState();
+  const [getrating, setgetrating] = useState(0);
 
   const findsentiment = () => {
     let result = sentiment.analyze(sentimenttext);
@@ -15,12 +17,18 @@ function App() {
 
     if (result.score < 0) {
       setGenealsentiment("Negative");
+      setgetrating( 5*(result.comparative * -1 ))
     } else if (result.score > 0) {
       setGenealsentiment("Positive");
+      setgetrating(5*result.comparative)
     } else {
       setGenealsentiment("Neutral");
+      setgetrating(5*result.comparative)
     }
     console.log(result);
+    // console.log(result.comparative);
+    // console.log(result);
+    // console.log(result);
   };
 
   return (
@@ -55,6 +63,17 @@ function App() {
       <p style={{ fontFamily: "cursive" }}>
         General Sentiment: {genealsentiment}
       </p>
+      <div style={{ fontFamily: "cursive" }}>
+      <StarRatings
+        rating={getrating}
+        // rating={1.5}
+        starDimension="21.5px"
+        starSpacing="1px"
+        starRatedColor="rgb(255,186,10)"
+        starEmptyColor="rgb(220,220,220)"
+        numberOfStars={5}
+      />
+      </div>
       {/* <a href="https://jsfiddle.net/bkanber/DHU7J/">
         Click this a sentimental analysis algo
       </a> */}
